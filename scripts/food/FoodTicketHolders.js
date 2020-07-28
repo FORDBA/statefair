d5e3063cea0a71cac5b1713efedcfcc32953b13b
FORDBA <bfordcpa@gmail.com>
2020-07-28 15:40:29 -0500

    initial commit

---
+++ scripts/food/FoodTicketHolders.js
@@ -0,0 +1,13 @@
+const contentTarget = document.querySelector(".food")
+const eventHub = document.querySelector("#state-fair")
+
+export const FoodTicketHolders = () => {
+    eventHub.addEventListener("foodTicketPurchased", customEvent => {
+    contentTarget.innerHTML += `<div class="person eater"></div>`
+
+    })
+    eventHub.addEventListener("fullPackageTicketPurchased", customEvent => {
+        contentTarget.innerHTML += `<div class="person bigSpender"></div>`
+    
+        })
+}
---
+++ scripts/sideshows/SideshowTicketHolders.js
@@ -0,0 +1,13 @@
+const contentTarget = document.querySelector(".sideshow")
+const eventHub = document.querySelector("#state-fair")
+
+export const SideshowTicketHolders = () => {
+    eventHub.addEventListener("sideshowTicketPurchased", customEvent => {
+    contentTarget.innerHTML += `<div class="person gawker"></div>`
+
+    })
+    eventHub.addEventListener("fullPackageTicketPurchased", customEvent => {
+        contentTarget.innerHTML += `<div class="person bigSpender"></div>`
+    
+        })
+}
